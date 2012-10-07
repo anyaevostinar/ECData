a2baa675e2ed29d5d83ce9047c5c0f4b05be2d7b
Anya Johnson <anyaejo@gmail.com>
2012-10-07 12:37:36 -0400

    added return statements

--- 001/config/msg_analyzer.py
+++ 001/config/msg_analyzer.py
@@ -23,14 +23,13 @@ def ourNot(notinputs):
             else:
                 sol+='0'
         not_solutions.append(sol)
+        return not_solutions
 
-#pos_solutions = ourNot(inputs)
 
 #Nand - if both inputs are 1 the output is 0, else the output is 1
 
 def ourNand(nandinput1, nandinput2):
-    #print "\nourNand - input 1  -  ", nandinput1
-    #print "ourNand - input 2  -  ", nandinput2
+
     nand_solution = []
     sol = ''
     for i in range(0, len(nandinput1)):
@@ -39,14 +38,13 @@ def ourNand(nandinput1, nandinput2):
         else:
             sol += '1'
     nand_solution.append(sol)
-    #print "ourNand - solution -", nand_solution
-#pos_solutions = ourNand(inputs[1], inputs[2])
+    return nand_solution
+
 
 #And
 
 def ourAnd(andinput1, andinput2):
-    #print "\nourAnd - input 1  -  ", andinput1
-    #print "ourAnd - input 2  -  ", andinput2
+
     and_solution = []
     sol = ''
     for i in range(0, len(andinput1)):
@@ -55,14 +53,11 @@ def ourAnd(andinput1, andinput2):
         else:
             sol += '0'
     and_solution.append(sol)
-    #print "ourAnd - solution -", and_solution
-#pos_solutions = ourAnd(inputs[1], inputs[2])
+    return and_solution
 
 #Or Not - an OR then a NOT
 
 def ourOrNot(ornotinput1, ornotinput2):
-    #print "\nourOrNot - input 1     -  ", ornotinput1
-    #print "ourOrNot - input 2     -  ", ornotinput2
     ornot_solution = []
     sol = ''
     for i in range(0, len(ornotinput1)):
@@ -71,15 +66,12 @@ def ourOrNot(ornotinput1, ornotinput2):
         else:
             sol += '1'
     ornot_solution.append(sol)
-    #print "ourOrNot - solution    -", ornot_solution
-
-#pos_solutions = ourOrNot(inputs[1], inputs[2])
+    return ornot_solution
 
 #Or
 
 def ourOr(orinput1, orinput2):
-    #print "\nourOr - input 1     -  ", orinput1
-    #print "ourOr - input 2     -  ", orinput2
+
     or_solution = []
     sol = ''
     for i in range(0, len(orinput1)):
@@ -88,14 +80,12 @@ def ourOr(orinput1, orinput2):
         else:
             sol += '0'
     or_solution.append(sol)
-    #print "ourOr - solution    -", or_solution
-#pos_solutions = ourOr(inputs[1], inputs[2])
+    return or_solution
 
 #And Not
 
 def ourAndNot(andnotinput1, andnotinput2):
-    #print "\nourAndNot - input 1  -  ", andnotinput1
-    #print "ourAndNot - input 2  -  ", andnotinput2
+
     andnot_solution = []
     sol = ''
     for i in range(0, len(andnotinput1)):
@@ -104,14 +94,12 @@ def ourAndNot(andnotinput1, andnotinput2):
         else:
             sol += '1'
     andnot_solution.append(sol)
-    #print "ourAndNot - solution -", andnot_solution
-#pos_solutions = ourAndNot(inputs[1], inputs[2])
+    return andnot_solution
 
 #Not Or - a NOT then an OR
 
 def ourNotOr(notorinput1, notorinput2):
-    #print "\nourNotOr - input 1     -  ", notorinput1
-    #print "ourNotOr - input 2     -  ", notorinput2
+
     notor_solution = []
     sol = ''
     for i in range(0, len(notorinput1)):
@@ -120,15 +108,12 @@ def ourNotOr(notorinput1, notorinput2):
         else:
             sol += '0'
     notor_solution.append(sol)
-    #print "ourNotOr - solution    -", notor_solution
+    return notor_solution
 
-#pos_solutions = ourNotOr(inputs[1], inputs[2])
-
-#Exclusive Or - if the inputs don't match the output is 0
+#Exclusive Or - if the inputs don't match the output is 1
 
 def ourXor(xorinput1, xorinput2):
-    #print "\nourXor - input 1  -  ", xorinput1
-    #print "ourXor - input 2  -  ", xorinput2
+
     xor_solution = []
     sol = ''
     for i in range(0, len(xorinput1)):
@@ -137,14 +122,12 @@ def ourXor(xorinput1, xorinput2):
         else:
             sol += '1'
     xor_solution.append(sol)
-    #print "ourXor - solution -", xor_solution
-#pos_solutions = ourXor(inputs[1], inputs[2])
+    return xor_solution
 
 #Equals - bitwise equal for each position, 1 if the positions match, 0 if not
 
 def ourEquals(eqinput1, eqinput2):
-    #print "\nourEquals - input 1  -  ", eqinput1
-    #print "ourEquals - input 2  -  ", eqinput2
+
     eq_solution = []
     sol = ''
     for i in range(0,len(eqinput1)):
@@ -153,8 +136,9 @@ def ourEquals(eqinput1, eqinput2):
         else:
             sol += '0'
     eq_solution.append(sol)
-    #print "ourEquals - solution -", eq_solution
-#pos_solutions = ourEquals(inputs[1], inputs[2])
+    return eq_solution
+
+
 
 folders = []
 
